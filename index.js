39f2af9d224c88cb42e0da321fd9e3cfa80dc5f4
mrwerner392 <mrwerner392@gmail.com>
2019-09-14 22:23:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event_name) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`)
+  }
+  return messages
+}
+
+function countdown(int) {
+  let counter = int;
+  while (counter >= 0) {
+    console.log(counter);
+    counter --;
+  }
+}
